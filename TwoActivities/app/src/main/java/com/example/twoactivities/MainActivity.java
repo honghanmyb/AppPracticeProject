6b4748a591bd3a1220663f210d1f2fba8f693130
honghanmyb <han.hong@mail.utoronto.ca>
2019-01-16 16:49:58 -0500

    Stroe data and restore in activity

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -18,10 +18,69 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG,"--------");
+        Log.d(LOG_TAG,"onCreate");
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.textMessageReply);
+
+        if(savedInstanceState != null){
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+
+            if(isVisible){
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+
+    }
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(mReplyHeadTextView.getVisibility() == View.VISIBLE){
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());
+        }
     }
 
     public void launchSecondActivity(View view) {
--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -3,16 +3,21 @@ package com.example.twoactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     private EditText mReply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "-------Second-----");
+        Log.d(LOG_TAG, "onCreate");
+        Log.d(LOG_TAG, LOG_TAG);
         setContentView(R.layout.activity_second);
         mReply = findViewById(R.id.editText_second);
         Intent intent = getIntent();
@@ -20,12 +25,49 @@ public class SecondActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textMessage);
         textView.setText(message);
     }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
 
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+
+    }
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+
+    }
     public void returnReply(View view) {
         String reply = mReply.getText().toString();
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY,reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG,"End second activity");
         finish();
     }
 }
